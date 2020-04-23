46dc4995bd1d6029857e6751605c904f6049c777
ztosyl <ztsapos@gmail.com>
2020-04-23 10:42:32 -0400

    Add tie functionality
    Made game recognize a full board as a tie.
    Made game display 'You tied!' in the event of a tie.
    Made game end in the event of a tie.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,15 +1,24 @@
-const getFormFields = require('../../../lib/get-form-fields')
+// const getFormFields = require('../../../lib/get-form-fields')
 const func = require('./functions')
-const api = ('./api')
-const ui = ('./ui')
+const api = require('./api')
+const ui = require('./ui')
 
 const addLetter = function (event) {
+  $('.message').text('')
   const game = func.currGame
   if ($(event.target).text() === '') {
     $(event.target).text(game.currPlayer)
     func.currGame.tray[$(event.target).data('id')] = game.currPlayer
-    if (game.whoWon() !== null) {
-      $('.who-won').text(`Player ${game.currPlayer} won!`)
+    const over = game.isOver()
+    // api.update($(event.target).data('id'), game.currPlayer, over)
+    //   .then(ui.updateSuccess)
+    //   .catch(ui.updateFailure)
+    if (over) {
+      if (game.isDraw) {
+        $('.who-won').text('You tied!')
+      } else {
+        $('.who-won').text(`Player ${game.currPlayer} won!`)
+      }
     } else if (game.currPlayer === 'X') {
       game.currPlayer = 'O'
     } else {
@@ -18,10 +27,14 @@ const addLetter = function (event) {
   }
 }
 
-const restartGame = function () {
-  func.currGame = new func.GameBoard(['', '', '', '', '', '', '', '', ''], 'X')
+const restartGame = function (event) {
+  func.currGame = new func.GameBoard(['', '', '', '', '', '', '', '', ''], 'X', false)
   $('.col-4').text('')
   $('.who-won').text('')
+  event.preventDefault()
+  api.create()
+    .then(ui.createGameSuccess)
+    .catch(ui.createGameFailure)
 }
 
 module.exports = {
--- assets/scripts/game/functions.js
+++ assets/scripts/game/functions.js
@@ -1,31 +1,35 @@
-function GameBoard (tray, currPlayer) {
+function GameBoard (tray, currPlayer, isDraw) {
   this.tray = tray
   this.currPlayer = currPlayer
+  this.isDraw = isDraw
 }
 
-GameBoard.prototype.whoWon = function () {
+GameBoard.prototype.isOver = function () {
   if (this.tray[0] !== '' && this.tray[0] === this.tray[1] && this.tray[0] === this.tray[2]) {
-    return this.tray[0]
+    return true
   } else if (this.tray[3] !== '' && this.tray[3] === this.tray[4] && this.tray[3] === this.tray[5]) {
-    return this.tray[3]
+    return true
   } else if (this.tray[6] !== '' && this.tray[6] === this.tray[7] && this.tray[6] === this.tray[8]) {
-    return this.tray[6]
+    return true
   } else if (this.tray[0] !== '' && this.tray[0] === this.tray[3] && this.tray[0] === this.tray[6]) {
-    return this.tray[0]
+    return true
   } else if (this.tray[1] !== '' && this.tray[1] === this.tray[4] && this.tray[1] === this.tray[7]) {
-    return this.tray[1]
+    return true
   } else if (this.tray[2] !== '' && this.tray[2] === this.tray[5] && this.tray[2] === this.tray[8]) {
-    return this.tray[2]
+    return true
   } else if (this.tray[0] !== '' && this.tray[0] === this.tray[4] && this.tray[0] === this.tray[8]) {
-    return this.tray[0]
+    return true
   } else if (this.tray[2] !== '' && this.tray[2] === this.tray[4] && this.tray[2] === this.tray[6]) {
-    return this.tray[2]
+    return true
+  } else if (this.tray.indexOf('') === -1) {
+    this.isDraw = true
+    return true
   } else {
-    return null
+    return false
   }
 }
 
-let currGame = new GameBoard(['', '', '', '', '', '', '', '', ''], 'X')
+let currGame = new GameBoard(['', '', '', '', '', '', '', '', ''], 'X', false)
 
 module.exports = {
   GameBoard,
